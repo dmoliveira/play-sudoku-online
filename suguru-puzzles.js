347da949a2e02b9cfd223bfc23f87e40691cbe1d
Diego Marinho <dmztheone@gmail.com>
2026-05-29 15:41:20 +1000

    Improve Suguru cage clarity

--- suguru-puzzles.js
+++ suguru-puzzles.js
@@ -1,26 +1,43 @@
 (function () {
-  const SHARED_CAGES = [
-    [0, 1, 2, 3, 4],
-    [5, 6, 7, 8, 9],
-    [10, 11, 12, 13, 14],
-    [15, 16, 17, 18, 19],
-    [20, 21, 22, 23, 24]
-  ];
-
-  const CAGE_MAP = Array.from({ length: 25 }, (_, index) => Math.floor(index / 5));
   const SOLUTION = "1234534512512342345151234";
 
+  const CAGE_LAYOUTS = {
+    breeze: [
+      [0, 1, 2, 3, 7],
+      [4, 8, 9, 13, 14],
+      [5, 6, 10, 11, 12],
+      [15, 16, 17, 20, 21],
+      [18, 19, 22, 23, 24]
+    ],
+    lantern: [
+      [0, 1, 5, 6, 7],
+      [2, 3, 4, 8, 9],
+      [10, 11, 15, 16, 17],
+      [12, 13, 14, 18, 19],
+      [20, 21, 22, 23, 24]
+    ]
+  };
+
+  function buildCageMap(cages) {
+    const map = Array(25).fill(-1);
+    cages.forEach((cage, cageIndex) => {
+      cage.forEach((cellIndex) => {
+        map[cellIndex] = cageIndex;
+      });
+    });
+    return map;
+  }
+
   function buildEntry(entry) {
+    const cages = CAGE_LAYOUTS[entry.layout].map((cage) => [...cage]);
     return {
       ...entry,
       size: 5,
       maxValue: 5,
-      cages: SHARED_CAGES.map((cage) => [...cage]),
-      cageMap: [...CAGE_MAP],
+      cages,
+      cageMap: buildCageMap(cages),
       solution: SOLUTION,
-      difficultyScore: entry.difficultyScore,
-      clueCount: entry.puzzle.split("").filter((value) => value !== "0").length,
-      estimatedMinutes: entry.estimatedMinutes
+      clueCount: entry.puzzle.split("").filter((value) => value !== "0").length
     };
   }
 
@@ -29,16 +46,18 @@
       buildEntry({
         id: "suguru-size5-garden-path",
         label: "Garden path",
-        puzzle: "1030504010012040040101030",
-        tags: ["starter", "row-cages"],
+        layout: "breeze",
+        puzzle: "1230530000000300005001000",
+        tags: ["starter", "walkthrough-shape"],
         estimatedMinutes: 5,
         difficultyScore: 1
       }),
       buildEntry({
         id: "suguru-size5-morning-rhythm",
         label: "Morning rhythm",
-        puzzle: "1004504010010040345001004",
-        tags: ["starter", "calm-scan"],
+        layout: "lantern",
+        puzzle: "1234000002000300005001000",
+        tags: ["starter", "irregular-cages"],
         estimatedMinutes: 6,
         difficultyScore: 2
       })
@@ -47,15 +66,17 @@
       buildEntry({
         id: "suguru-size5-lantern-challenge",
         label: "Lantern challenge",
-        puzzle: "1234000010000300005001000",
-        tags: ["challenge", "focused"],
+        layout: "breeze",
+        puzzle: "1230530000000040005001000",
+        tags: ["challenge", "walkthrough-shape"],
         estimatedMinutes: 8,
         difficultyScore: 3
       }),
       buildEntry({
         id: "suguru-size5-quiet-koi",
         label: "Quiet koi",
-        puzzle: "1234000010000040005050000",
+        layout: "lantern",
+        puzzle: "1234000002000300005050000",
         tags: ["challenge", "sparse"],
         estimatedMinutes: 9,
         difficultyScore: 4
